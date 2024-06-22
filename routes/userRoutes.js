30d023e609ccc5448398c8791501ca0c657c9bb4
JEEVAABI15 <jeevaabishake2002@gmail.com>
2024-06-22 21:59:01 +0530

    Added protect and admin auth in UserRoutes

--- routes/userRoutes.js
+++ routes/userRoutes.js
@@ -9,7 +9,7 @@ const {
   logoutUser,
 } = require('../controllers/userController');
 
-router.route('/').post(registerUser).get(getUsers);
+router.route('/').post(registerUser).get(protect, admin, getUsers);
 router.post('/login', authUser);
 router.get('/profile',protect, getUserProfile);
 router.get('/logout', logoutUser);
