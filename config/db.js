194fa992ea1a25616962ea65a4fb96d72aea8341
JEEVAABI15 <jeevaabishake2002@gmail.com>
2024-06-22 21:52:33 +0530

    DB updated

--- config/db.js
+++ config/db.js
@@ -2,6 +2,7 @@ const mongoose = require('mongoose');
 require('dotenv').config();
 const connectDB = async () => {
   try {
+    console.log('Attempting to connect to MongoDB...');
     const conn= await mongoose.connect(process.env.MONGODB_URL);
     console.log('MongoDB Connected...' + conn.connection.host , conn.connection.name);
     return conn.connection.name
